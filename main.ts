fcf151fc7b57a2b6cf022bee4b91d0b1dea04e7e
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-12 17:09:58 -0400

    Restore '12/09/2023, 16:51:47 fix readme'

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  
- * Created by: Mr. Coxall
+ * Created by: Cedric
  
  * Created on: Sep 2020
  
- * This program ...
+ * This program ... prints 'hello world' 
 
-basic.showString('Hello, World!')\
+basic.showString('Hello, World!')
+*/
