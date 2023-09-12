1da2215ea2cc6c40340637da9b27a4f74a160d6f
Cedric-dehghan-nejad <144264289+Cedric-dehghan-nejad@users.noreply.github.com>
2023-09-12 16:49:35 -0400

    Update main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  
- * Created by: Mr. Coxall
+ * Created by: cedric
  
  * Created on: Sep 2020
  
  * This program ...
 
-basic.showString('Hello, World!')\
+basic.showString('Hello, World!')
+*/
